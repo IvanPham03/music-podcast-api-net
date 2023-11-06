5b3c0ed4a013e42f87c2214c486dc7dfb946c765
truongpham <dangtruongphamvn@gmail.com>
2023-11-06 12:10:58 +0700

    'init'

---
+++ MusicPodcast/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using MusicPodcast.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MusicPodcast.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MusicPodcastDbContext _context;
+        public UserController(MusicPodcastDbContext _context)
+        {
+            this._context = _context;
+        }
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            return Ok(_context.User.ToList());
+        }
+    }
+}
---
+++ MusicPodcast/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicPodcast.Models
+{
+    public class User
+    {
+        [Key]
+        public long id { get; set; }
+        public string  userName { get; set; }
+        public string  email { get; set; }
+        public string  phoneNumber { get; set; }
+        public string  password { get; set; }
+        public string  pathImg { get; set; }
+        public DateTime timestamp { get; set; }
+        public string role { get; set; }
+    }
+}
